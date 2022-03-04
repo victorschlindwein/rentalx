55673d3ab6b51f095a31e1426046090d07ebdd0b
victorschlindwein <victorwilbert@gmail.com>
2022-03-04 19:18:43 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.json({ message: "maybe working" })
+});
+
+app.listen(3333, () => console.log("server is on"));
