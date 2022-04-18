69be2388251173df2943723442184352771e8a73
victorschlindwein <victorwilbert@gmail.com>
2022-04-18 14:55:55 -0300

    refactoring category useCase

--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -2,10 +2,12 @@ import { CategoriesRepository } from "../../repositories/implementations/Categor
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 import { CreateCategoryController } from "./CreateCategoryController"
 
-const categoriesRepository = CategoriesRepository.getInstance();
+export default (): CreateCategoryController => {
+  const categoriesRepository = new CategoriesRepository();
 
-const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
+  const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
+  
+  const createCategoryController = new CreateCategoryController(createCategoryUseCase);
 
-const createCategoryController = new CreateCategoryController(createCategoryUseCase)
-
-export { createCategoryController }
+  return  createCategoryController;
+};
--- src/modules/cars/useCases/listCategories/index.ts
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -2,7 +2,7 @@ import { ListCategoriesController } from "./ListCategoriesController";
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository"
 
-const categoriesRepository = CategoriesRepository.getInstance();
+const categoriesRepository = null;
 const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
 const listCategoriesController = new ListCategoriesController(listCategoriesUseCase);
 
